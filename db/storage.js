12619be803554375d0eefdcad68668976e7b94d2
Leah <88125500+Leah3232@users.noreply.github.com>
2022-03-14 09:50:42 -0700

    Add files via upload

---
+++ db/storage.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+// const uuidv1 = require('uuid/v1')
+const util = require('util')
+
+
+// using a class 
+// define read(), write(), getNotes() 
+// export that class
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  constructor() {
+    this.lastId = 0;
+  }
+
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then(notes => {
+      let parsedNotes;
+
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+
+   
+    const newNote = { title, text, id: ++this.lastId };
+
+    return this.getNotes()
+      .then(notes => [...notes, newNote])
+      .then(updatedNotes => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+  removeNote(id) {
+   
+    return this.getNotes()
+      .then(notes => notes.filter(note => note.id !== parseInt(id)))
+      .then(filteredNotes => this.write(filteredNotes));
+  }
+}
+
+module.exports = new Store();
